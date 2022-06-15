dceb57e3b3bb9a92ebdbf128885c8bd09f05fa21
Abass <Abassolanrewaju111@gmail.com>
2022-06-15 22:46:05 +0100

    design ui for Calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,73 +3,36 @@ import React from 'react';
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      output: 0,
+    };
   }
 
   render() {
+    const { output } = this.state;
     return (
-      <form>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-        <button type="button">
-          Hello World
-        </button>
-      </form>
+      <div className="calc-container">
+        <div className="digit display-output">{output}</div>
+        <div className="digit">AC</div>
+        <div className="digit">+/-</div>
+        <div className="digit">%</div>
+        <div className="digit">÷</div>
+        <div className="digit ">7</div>
+        <div className="digit">8</div>
+        <div className="digit">9</div>
+        <div className="digit">x</div>
+        <div className="digit">4</div>
+        <div className="digit">5</div>
+        <div className="digit">6</div>
+        <div className="digit">-</div>
+        <div className="digit">1</div>
+        <div className="digit">2</div>
+        <div className="digit">3</div>
+        <div className="digit">+</div>
+        <div className="digit big-digit">0</div>
+        <div className="digit">.</div>
+        <div className="digit">=</div>
+      </div>
     );
   }
 }
