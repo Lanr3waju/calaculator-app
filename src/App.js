ec314ce0938f412b2073d5071f23b613026c7dee
Abass <Abassolanrewaju111@gmail.com>
2022-06-15 21:15:13 +0100

    create buttons for calculator

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Calculator from './components/Calculator';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Calculator />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <form>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+        <button type="button">
+          Hello World
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Calculator;
