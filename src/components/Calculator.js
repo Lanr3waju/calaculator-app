d9b624e06cf0d8f0ecf7efb5550c9e71d0ef3772
Abass Abdul Wasi Olanrewaju <abassolanrewaju111@gmail.com>
2022-06-18 19:47:43 +0100

    Add calculator logic (#2)
    * Add logic functions for the calculator app

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,45 +1,71 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      output: 0,
+      total: 0,
+      next: null,
+      operation: null,
+
     };
   }
 
-  render() {
-    const { output } = this.state;
-    const inputs = [
-      output,
-      'AC',
-      '+/-',
-      '%',
-      '÷',
-      7,
-      8,
-      9,
-      'x',
-      4,
-      5,
-      6,
-      '-',
-      1,
-      2,
-      3,
-      '+',
-      0,
-      '.',
-      '=',
-    ];
-    return (
-      <div className="calc-container">
-        {inputs.map((input) => (
-          <div key={input} className="digit">{input}</div>
-        ))}
-      </div>
-    );
+  changeOutput = (event) => {
+    this.setState((prevState) => (
+      calculate(prevState, event.target.name)
+    ));
   }
+
+   showTotal = () => {
+     const { total, next } = this.state;
+     if (total === 0 && next === null) {
+       return 0;
+     } if (total !== 0 && total !== null) {
+       return total;
+     }
+     return '';
+   };
+
+   render() {
+     const { operation, next } = this.state;
+     const inputs = [
+       'AC',
+       '+/-',
+       '%',
+       '÷',
+       '7',
+       '8',
+       '9',
+       'x',
+       '4',
+       '5',
+       '6',
+       '-',
+       '1',
+       '2',
+       '3',
+       '+',
+       '0',
+       '.',
+       '=',
+     ];
+     return (
+       <>
+         <header>
+           <h1>the simple react calculator</h1>
+         </header>
+         <form className="calc-container">
+           <input className="digit" type="text" key={uuidv4()} value={(this.showTotal()) + (operation || '') + (next || '')} disabled />
+           {inputs.map((input) => (
+             <button key={uuidv4()} name={input} type="button" onClick={this.changeOutput} className="digit">{input}</button>
+           ))}
+         </form>
+       </>
+     );
+   }
 }
 
 export default Calculator;
